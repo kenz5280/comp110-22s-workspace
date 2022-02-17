8eb2f96d6dd08a52f74d42555867cec6920b6278
Kenzie Talhelm <ktalhelm@unc.edu>
2022-02-17 10:37:09 -0500

    work in class

---
+++ lessons/contains.py
@@ -0,0 +1,29 @@
+"""Example of writing a function to search a list."""
+
+# define a function named contains
+# parameters:
+#   1. needle - the str we're searching for
+#   2. haystack - the list of str values we're searching through
+
+
+def main() -> None:
+    guess: str = input("What is the code word? ")
+    possible_answers: list[str] = ["pokemon", "wordle"]
+    if contains(guess, possible_answers):
+        print("We're letting you in the secret club...")
+    else:
+        print("Go back to Davis")
+
+
+def contains(needle: str, haystack: list[str]) -> bool:
+    """Test if needle is found in haystack."""
+    for item in haystack: 
+        if item == needle: 
+            return True
+    return False
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    main()
