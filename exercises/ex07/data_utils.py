08c6f8fd885e67eb11b6189caa8bfcd5d469e8b6
Kenzie Talhelm <ktalhelm@unc.edu>
2022-03-10 09:37:38 -0500

    work on ex07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -3,3 +3,33 @@
 __author__ = ""
 
 # Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a table."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column_name: str) -> list[str]:
+    """Produces a list of values for a single column when given column name."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column_name]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transforms a table from a list of rows to a dictionary of columns."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
