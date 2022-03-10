7efd457e5dda0f2df7c363d6ae5ec89179fe38e1
Kenzie Talhelm <ktalhelm@unc.edu>
2022-03-10 17:51:33 -0500

    Work on exercise 7

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -33,3 +33,48 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     for column in first_row:
         result[column] = column_values(row_table, column)
     return result
+
+
+def head(table: dict[str, list[str]], number_rows: int) -> dict[str, list[str]]:
+    """A function that produces a new table with only the first N data selected for each column."""
+    result: dict[str, list[str]] = {}
+    for columns in table:
+        first_n_values: list[str] = []
+        i: int = 0 
+        for i in range(0, number_rows):
+            first_n_values.append(table[columns][i])
+            result[columns] = first_n_values
+    return result
+        
+
+def select(table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """A function that produces a column-based table w/ only a select group of orig. columns."""
+    result: dict[str, list[str]] = {}
+    for names in column_names:
+        if names in table:
+            result[names] = table[names]
+    return result
+
+
+def concat(a_dict: dict[str, list[str]], b_dict: dict[str, list[str]]) -> dict[str, list[str]]:
+    """A function that produces a new table that combines two other tables."""
+    result: dict[str, list[str]] = {}
+    for columns in a_dict:
+        result[columns] = a_dict[columns]
+    for columns in b_dict:
+        if columns in result:
+            result[columns] += b_dict[columns]
+        else:
+            result[columns] = b_dict[columns]
+    return result
+
+
+def count(a_list: list[str]) -> dict[str, int]:
+    """A function that provides a count of values from given list."""
+    result: dict[str, int] = {}
+    for item in a_list:
+        if item in result:
+            result[item] += 1
+        else: 
+            result[item] = 1
+    return result
