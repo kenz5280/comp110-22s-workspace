45b508e492323608fc5490d8c4ea5f7a203c6571
Kenzie Talhelm <ktalhelm@unc.edu>
2022-04-19 10:25:39 -0400

    Work on ls31

---
+++ lessons/recursion.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import Optional
+
+
+class Node:
+    data: int
+    next: Optional[Node]
+
+    def __init__(self, data: int, next: Optional[Node]):
+        """Builds up structural recursion."""
+        self.data = data
+        self.next = next
+
+    def __str__(self) -> str:
+        """Recursive method call."""
+        if self.next is None:
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
+
+
+def sum(node: Optional[Node]) -> int:
+    """Procedural recursion; recursive function."""
+    if node is None:
+        return 0
+    else:
+        return node.data + sum(node.next)
+
+
+def count(node: Optional[Node], current_count: int = 0) -> int:
+    if node is None:
+        return current_count
+    else:
+        return count(node.next, current_count + 1)
+
+
+head: Node = Node(3, None)
+head: Node = Node(2, head)
+head = Node(1, head)
+print(sum(head))
+print(count(head))
+print(head)
